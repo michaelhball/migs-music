26913d06d29263eaf177157883090197796ef6d1
Michael Ball <michael.h.s.ball@gmail.com>
2026-09-06 23:22:15 +0200

    Bump version to 0.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         resValue("string", "app_name", "migs music")
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.2.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
